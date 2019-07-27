869916a1a58da29fa6d3e1b9e24222f12e2e713f
durongze <durongze@qq.com>
2019-07-27 14:25:59 +0100

    cmake install

--- cmake/002/main.c
+++ cmake/002/main.c
@@ -1,6 +1,18 @@
-#include "math.h"
 
-int main()
+#include "config.h"
+#ifdef USE_MYMATH
+#include "math.h"
+#else
+#include <stdio.h> 
+#endif
+int main(int argc, char *argv[])
 {
+#ifdef USE_MYMATH
     return add(1, 2);
+#else
+    printf("no surported. \n");
+    return 0;
+#endif
 }
+
+
