31b9e24c6d4ad87236567b2b365dad05f0f88e8f
durongze <durongze@qq.com>
2024-06-07 12:29:44 +0800

    Add files via upload

---
+++ cmake/dep_pdcurses.cmake
@@ -0,0 +1,14 @@
+if(NOT TARGET PDCurses)
+  set(LIBNAME PDCurses)
+  set(${LIBNAME}_DIR ${CMAKE_CURRENT_LIST_DIR}/../thirdparty/${LIBNAME})
+  message("${LIBNAME}_DIR:${${LIBNAME}_DIR}")
+  if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+    add_compile_definitions(DYZ)
+  endif()
+  add_subdirectory(${${LIBNAME}_DIR}/)
+endif()
+
+include_directories(${${LIBNAME}_DIR}/include ${${LIBNAME}_DIR}/ ${CMAKE_BINARY_DIR}/thirdparty/${LIBNAME})
+
+set(PDCurses_LIBRARY PDCurses_static)
+set(PDCurses_LIBRARIES $<TARGET_OBJECTS:PDCurses_static>)
